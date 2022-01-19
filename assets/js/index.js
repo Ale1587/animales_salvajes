c809216404e5e97eb575d8bf58a183d9638c1c39
Alejandra Valencia <valencialemus.a@gmail.com>
2022-01-19 17:18:39 -0300

    se agrega ventana modal y se revisan detalles del código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,30 @@
 import consulta from "./consulta.js";
-import { Leon, Lobo, Oso, Serpiente } from "./tipoAnimales.js";
+import { Leon, Lobo, Oso, Serpiente, Aguila } from "./tipoAnimales.js";
 
 
 const dom = (function () {
     // el profe creo una constante llamada estado para alojar las respuesta asincronas del json, que vienen del modulo 
     // consulta. Esa constante es un objeto, que dentro tiene un array vacio. la colocó afuera de toda función para que tuviera un scope mayor
 
+    
     const estado = {
-        animales: []
+        animales: [],
+        instancias: []
     }
+    
+    // DOmcache
+
+    const tipo = document.querySelector('#animal')
+    const edad_animal = document.querySelector('#edad')
+    const comentario = document.querySelector('#comentarios')
+    const tabla_html = document.querySelector('#Animales')
+    const img_container = document.querySelector('.p-5.card.bg-dark')
+    const form = document.querySelector('form')
+    const modal = document.querySelector('.modal-body')
+
+    
+    form.addEventListener('submit', SubmitHandler);
+    $('#modal').on('show.bs.modal', showModalHandler)
 
     // comprobación que consulta json funciona. accediendo a la consulta del json en el archivo importado y igualando la const estado con el resultado de la 
     // consulta
@@ -16,16 +32,15 @@ const dom = (function () {
     async function init() {
         const json = await consulta.getData()
         estado.animales = json.animales
-
     }
 
-    document.querySelector('#animal').addEventListener('change', () => {
+
+        tipo.addEventListener('change', () => {
         const { animales } = estado
-        const animal_valor = document.querySelector('#animal').value
+        const animal_valor = tipo.value
         const ImagenesTemplate = animales.find((e) => e.name == animal_valor).imagen
 
-        const preview = document.querySelector('.p-5.card.bg-dark')
-        preview.innerHTML = `<img src="assets/imgs/${ImagenesTemplate}" class="figure"/>`
+        img_container.innerHTML = `<img src="assets/imgs/${ImagenesTemplate}" class="figure"/>`
 
     })
 
@@ -33,8 +48,8 @@ const dom = (function () {
     // capturar el evento submit y crear la instancia animal
     // aquí creo una const del elemento form, para capturar el submit y luego le hizo a form el addEventListener
 
-    const form = document.querySelector('form')
-    form.addEventListener('submit', SubmitHandler);
+    
+    
 
 
     // en la función handler creo una variable instancia, para poder almacenar las instancias que vamos creando al elegir un animal
@@ -58,8 +73,10 @@ const dom = (function () {
         // en el if, voy viendo si la seleccion de option es igual al nombre del animal, creo la instancia de ese animal
 
         if (form_data.edad === 'Seleccione un rango de años' || form_data.comentario === '') {
-            return alert ('favor complete el formulario ')
-        } else if (form_data.nombre === 'Leon') {
+            return alert ('favor complete el formulario ')} 
+            
+            
+        if (form_data.nombre === 'Leon') {
             instancia = new Leon(form_data.nombre, form_data.edad, form_data.imagen, form_data.comentario, form_data.sonido)
         } else if (form_data.nombre === 'Lobo') {
             instancia = new Lobo(form_data.nombre, form_data.edad, form_data.imagen, form_data.comentario, form_data.sonido)
@@ -70,21 +87,43 @@ const dom = (function () {
         } else if (form_data.nombre == 'Aguila') {
             instancia = new Aguila(form_data.nombre, form_data.edad, form_data.imagen, form_data.comentario, form_data.sonido)
         }
-    
-        const card = `<div class="card-group" style="width: 18rem;">
-        <img src="./assets/imgs/${instancia.img}" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">${instancia.sonido}</div>
-      </div>`
+        
+        estado.instancias.push(instancia)
+        const card = htmlCard(instancia, estado.instancias.length - 1) 
+        
+        tabla_html.innerHTML += card
 
-      const tabla_html = document.querySelector('#Animales')
-      tabla_html.innerHTML += card
-
-      document.querySelector('form').reset()
+        form.reset()
+        img_container.innerHTML = '<div id="preview" name="preview"></div>'
       
     }
 
 
-    // generar un modal en base a Animal
+    function htmlCard (instancia, index){
+        return `<div class="card-group" style="width: 18rem;">
+        <img src="./assets/imgs/${instancia.img}" class="card-img-top img-fluid" alt="..." data-toggle="modal" data-target="#modal" data-index ="${index}"
+        >
+        <div class="card-body">${instancia.sonido}</div>
+        </div>`
+    }
+
+    function htmlModal (animal){
+
+        console.log(animal.edad);
+        return  `<div class="card" style="width: 18rem;">
+        <img src="./assets/imgs/${animal.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p>${animal.edad}</p>
+            <p>${animal.comentarios}</p>
+        </div>
+      </div>`
+    }
+   
+    function showModalHandler(e) {
+        const animal = estado.instancias[e.relatedTarget.dataset.index]
+        modal.innerHTML = htmlModal(animal)
+    }
+
 
     return { init: init };
 
